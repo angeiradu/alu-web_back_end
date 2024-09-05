b6e45c2ef43476b43ade416a65b7d03c537036c1
angeiradu <a.iradukund2@alustudent.com>
2024-09-05 18:00:48 +0300

    Complex types - string and int/float to tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""define function to_kv"""
+
+
+from ast import Tuple
+from ctypes import Union
+
+
+def to_kv(K: str, v: Union[int, float]) -> Tuple[str, float]:
+    """return a tuple"""
+    return (k, v * v)
